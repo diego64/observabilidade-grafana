947e8247e3a59d6328e7f38d2601f0463e21fa24
Diego Ferreira L.G.Oliveira <diegoferreira1964@gmail.com>
2025-09-26 18:05:03 -0300

    fix: correcao na rota

--- application-02/src/domain/users/user.module.ts
+++ application-02/src/domain/users/user.module.ts
@@ -3,7 +3,7 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [UserModule],
+  //imports: [UserModule],
   controllers: [UserController],
   providers: [UserService],
 })
