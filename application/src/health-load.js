0f9cbb162d733ac577b8f009e92bf45bedee0294
Diego Ferreira L.G.Oliveira <diegoferreira1964@gmail.com>
2025-09-26 13:25:40 -0300

    perf: automacao de envio de requisicoes

---
+++ application/src/health-load.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const URL = "http://localhost:3001/health";
+
+async function callHealth() {
+  // gera quantas requisições enviar: entre 1 e 100
+  const requestsCount = getRandomInt(1, 100);
+  console.log(`[${new Date().toISOString()}] Enviando ${requestsCount} requisições...`);
+
+  for (let i = 0; i < requestsCount; i++) {
+    const randomNumber = getRandomInt(1, 200);
+
+    try {
+      const response = await axios.get(URL, {
+        params: { value: randomNumber },
+      });
+
+      console.log(
+        `[${new Date().toISOString()}] Enviado: ${randomNumber} | Resposta: ${response.data}`
+      );
+    } catch (err) {
+      console.error(`[${new Date().toISOString()}] Erro:`, err.message);
+    }
+  }
+
+  // espera aleatória de 0 a 10s antes da próxima execução
+  const delay = getRandomInt(0, 10_000);
+  console.log(`Próxima execução em ${(delay / 1000).toFixed(0)}s...\n`);
+
+  setTimeout(callHealth, delay);
+}
+
+callHealth();
